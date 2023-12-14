d4a26ef5aafdcf43510eae65e0313364477ad675
Danny <dannywo@gmail.com>
2023-12-13 23:51:38 -0800

    Creates nav menu

---
+++ app/NavMenu.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from './NavMenu.module.css'
+
+export default function NavMenu() {
+    return (
+        <nav className={styles.nav}>
+            <Image src="logo.svg" alt='NextSpace Logo' width={216} height={30} />
+        </nav>
+    )
+}
