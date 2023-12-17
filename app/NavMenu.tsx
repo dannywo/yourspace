1828d7cbbce5434badecfeb868538b2a5f934db6
Danny <dannywo@gmail.com>
2023-12-16 16:36:18 -0800

    updates menu items

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -4,9 +4,21 @@ import styles from './NavMenu.module.css'
 
 export default function NavMenu() {
     return (
-        <nav className={styles.nav}>
-            <Image src="logo.svg" alt='NextSpace Logo' width={216} height={30} />
-            <a>Test</a>
+        // <nav className={styles.nav}>
+        <nav className="flex content-center justify-between bg-cyan-700">
+            <Link href={"/"}>
+
+                <Image src="next.svg"
+                    alt='NextSpace Logo'
+                    width={216}
+                    height={30} />
+            </Link>
+
+            <ul className=''>
+                <li><Link href={'/about'}>About</Link></li>
+                <li><Link href={'/blog'}>Blog</Link></li>
+                <li><Link href={'/users'}>Users</Link></li>
+            </ul>
         </nav>
     )
 }
