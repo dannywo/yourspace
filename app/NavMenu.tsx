3a7b0fe4cfcc1fd925d91f9c71f62e3d36a872d3
Danny <dannywo@gmail.com>
2023-12-19 00:11:21 -0800

    nav classname uses str literals

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -5,16 +5,14 @@ import styles from './NavMenu.module.css'
 export default function NavMenu() {
     return (
         // <nav className={styles.nav}>
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <nav className={`bg-white border-gray-200 dark:bg-gray-900`}>
             <div className='<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-
                 <Link href={"/"}>
-
                     <Image src="next.svg"
                         alt='NextSpace Logo'
                         width={216}
                         height={30}
-                        className='white' />
+                        className={styles.filterWhite} />
                 </Link>
 
                 <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 text-white'>
