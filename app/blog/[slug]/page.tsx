4301eb0a13404480ace83746850c6c60df16d2e7
Danny <dannywo@gmail.com>
2023-12-21 23:53:36 -0800

    slight troubleshooting, in progress.

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,22 +1,28 @@
 interface Post {
-    title: string;
-    content: string;
-    slug: string;
+    title: string,
+    content: string,
+    slug: string,
 }
 
 interface Props {
-    params: { slug: string; }
+    params: { slug: string }
 }
 
 export default async function BlogPostPage({ params }: Props) {
     const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
         (res) => res.json()
     );
+    console.log(params)
+    console.log('--------')
+    console.log(posts)
     const post = posts.find((post) => { post.slug === params.slug })!;
     console.log(post)
-    return (
-        <div>'hello' < /div>
-    )
+    // return (
+    //     <div>
+    //     <h1>{ post.ti } < /h1>
+    //     < p > { post.content } < /p>
+    //     < /div>
+    // )
     // < div >
     // <h1>{ post.title } < /h1>
     // < p > { post.content } < /p>
