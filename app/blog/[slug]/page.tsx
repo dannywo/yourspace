836c6b260ff150bd019f0631c0fdc994d5e92cd2
Danny <dannywo@gmail.com>
2023-12-25 13:23:06 -0800

    Linter fixed, post is still not found

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -12,22 +12,23 @@ export default async function BlogPostPage({ params }: Props) {
     const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
         (res) => res.json()
     );
+
+    const listPosts: any = posts.map((post) => {
+        return (<li key={post.slug}>
+            <h1>{post.title} </h1>
+            <p> {post.content} </p>
+        </li>)
+    })
     console.log(params)
     console.log('--------')
     console.log(posts)
-    const post = posts.find((post) => { post.slug === params.slug })!;
+    const post = posts.find((post) => { post.slug == params.slug })!;
     console.log(post)
-    // return (
-    //     <div>
-    //     <h1>{ post.ti } < /h1>
-    //     < p > { post.content } < /p>
-    //     < /div>
-    // )
-    // < div >
-    // <h1>{ post.title } < /h1>
-    // < p > { post.content } < /p>
-    // < /div>
-
-
-
+    return (
+        <div>
+            {/* <ul>{listPosts}</ul> */}
+            <h1>{post.title} </h1>
+            <p> {post.content} </p>
+        </div>
+    )
 }
