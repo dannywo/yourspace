cb88a17bb421589a37315e7270daaa3db7a7cd52
Danny <dannywo@gmail.com>
2023-12-27 23:03:21 -0800

    integrate serverside auth

--- app/api/content/route.ts
+++ app/api/content/route.ts
@@ -1,21 +1,24 @@
+import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 
 // Dummy data
 const posts = [
-  {
-    title: 'Lorem Ipsum',
-    slug: 'lorem-ipsum',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
-  },
-  {
-    title: 'Where is Waldito?',
-    slug: 'waldo',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
-  },
+    {
+        title: 'Lorem Ipsum',
+        slug: 'lorem-ipsum',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
+    },
+    {
+        title: 'Where is Waldito?',
+        slug: 'waldo',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
+    },
 ];
 
 export async function GET() {
-  return NextResponse.json(posts);
+    const session = getServerSession();
+
+    return NextResponse.json(posts);
 }
