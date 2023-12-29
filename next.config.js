c6d782b28fe014bde0506157c5495954a620af84
Danny <dannywo@gmail.com>
2023-12-29 00:19:11 -0800

    configures images settings for auth user

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        appDir: true
+    }, images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+                pathname: '/u/**'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
