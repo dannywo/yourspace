ae46efe9872a63908cee32028f3e37c5ada288bb
Danny <dannywo@gmail.com>
2023-12-29 13:16:34 -0800

    adds force siging - protect route

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation';
 import Image from 'next/image'
 
 export default function Home() {
+  const session = getServerSession();
+
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
