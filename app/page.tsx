a9b441eb0337a3509a6d64426ef5cf69cb657f02
Danny <dannywo@gmail.com>
2023-12-30 14:22:56 -0800

    Update home to async

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation';
 import Image from 'next/image'
 
-export default function Home() {
-  const session = getServerSession();
+export default async function Home() {
+  const session = await getServerSession();
+
+  console.log('Current session in home: ', session);
 
   if (!session) {
     redirect('/api/auth/signin');
@@ -11,7 +13,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <h1>Home Page</h1>
     </main>
   )
 }
