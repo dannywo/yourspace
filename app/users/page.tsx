38d18e2d8ca74b0088f3c36317069298b36888ca
Danny <dannywo@gmail.com>
2024-01-15 13:06:49 -0800

    Add css styling to users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,13 @@
 import UserCard from '@/components/UserCard/UserCard';
-// import styles from './page.module.css';
+import styles from './page.module.css';
 import { prisma } from "@/lib/prisma";
 
 export default async function Users() {
+    // throw new Error('This is an error.')
     const users = await prisma.user.findMany();
     return (
 
-        <div className="">
+        <div className={styles.grid}>
             {users.map((user) => {
                 return <UserCard key={user.id} {...user} />;
             })}
