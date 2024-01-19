af6d5c433fb590753566d5ab6028d6a8b8b2cfcb
Danny <dannywo@gmail.com>
2024-01-18 23:00:04 -0800

    Adds route function to delete/unfollow user

--- app/api/follow/route.ts
+++ app/api/follow/route.ts
@@ -25,3 +25,28 @@ export async function POST(req: Request) {
 
     return NextResponse.json(record);
 }
+
+export async function DELETE(req: NextRequest) {
+    const session = await getServerSession(authOptions);
+    const currentUserEmail = session?.user?.email!;
+    const targetUserId = req.nextUrl.searchParams.get('targetUserId');
+
+    const currentUserId = await prisma.user
+        .findUnique({
+            where: {
+                email: currentUserEmail,
+            },
+        })
+        .then((user) => user?.id!);
+
+    const record = await prisma.follows.delete({
+        where: {
+            followerId_followingId: {
+                followerId: currentUserId,
+                followingId: targetUserId!,
+            },
+        },
+    });
+
+    return NextResponse.json(record);
+}
